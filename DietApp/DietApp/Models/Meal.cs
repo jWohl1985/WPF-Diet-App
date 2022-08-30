feae216f388017bb370b8fca11269d6ffe31f47a
Jeremy Wohl <jdwohl@gmail.com>
2022-08-29 19:16:11 -0700

    Meal model
    Adds model/test for meal class

---
+++ DietApp/DietApp/Models/Meal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DietApp.Models
+{
+    public class Meal
+    {
+        public float Servings { get; private set; }
+        public Food Food { get; private set; }
+        public DayOfWeek DayOfWeek { get; private set; }
+
+        public Meal(float servings, Food food, DayOfWeek dayOfWeek)
+        {
+            this.Servings = servings;
+            this.Food = food;
+            this.DayOfWeek = dayOfWeek;
+        }
+    }
+}
